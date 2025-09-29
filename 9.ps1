026c927c2b7a38a7201e2997b3d8c782c018753e
ivanortigosa <garethivanobalic@gmail.com>
2025-09-29 11:01:48 +0200

    añadiendo banderas

---
+++ 9.ps1
@@ -0,0 +1,21 @@
+#######################################
+#######   BUSCAR UN PROCESO	    #######
+#######################################
+
+param (
+$proceso
+)
+$f=0
+$p=get-process
+$l=$p.length
+for ($i=0;$i -lt $l;$i++)
+{
+if ($p[$i].ProcessName -eq $proceso)
+{
+    $f=1
+}
+}
+if ($f -eq 1)
+    {write-host "Proceso " $proceso " Encontrado"}
+else
+    {write-host "Proceso " $proceso " no encontrado"}
--- tabla.ps1
+++ tabla.ps1
@@ -3,5 +3,6 @@
 #######################################
 
 param (
-$numero
-)for ($i=1; $i -lt 11; $i++){$r=$numero*$iwrite-host $r}
+$numero,
+$final
+)for ($i=1; $i -lt $final; $i++){$r=$numero*$iwrite-host $r}
